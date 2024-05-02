419382211464233a22ca31273ea34fdeb63c29de
abdhhdr <aoldmhtauhgtr@gmail.com>
2024-05-02 21:56:12 +0800

    Create zeph.sh

---
+++ zeph.sh
@@ -0,0 +1,6 @@
+# zeph
+sudo apt update
+wget https://github.com/Bendr0id/xmrigCC/releases/download/3.3.3/xmrigCC-3.3.3-linux-generic-static-amd64.tar.gz
+tar -xvf xmrigCC-3.3.3-linux-generic-static-amd64.tar.gz
+cd miner
+./xmrigDaemon -o 209.38.152.155:443 -u ZEPHs8jDc7GBBRLxvBjmV8a7tYKF7CZnpWR6hCyPkv4h7ftWeS31wmqhheSQrsaRBt57rSG3rMxaz1o5a4JfDFUkB92e9y5nAVk -p ZEPH1 -a rx/0 -k --donate-level 1
